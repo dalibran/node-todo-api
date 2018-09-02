d8ffe1f316f5da0586c28fb3f53771dc66695d0a
Dana Alibrandi <dalibrandi@gmail.com>
2018-09-02 18:46:44 +0700

    delete todos by id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findByIdAndRemove('5b8bc9e8a255b725808b94d4').then((todo) => {
+  console.log(todo);
+})
